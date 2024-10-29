def1b5f5471872f482c722ca9ec6f87b29344016
CLyieee <130021408+CLyieee@users.noreply.github.com>
2024-10-30 00:38:45 +0800

    Update index.js

--- a-service/index.js
+++ a-service/index.js
@@ -4,38 +4,38 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { Server } = require("socket.io");
-const orderRoutes = require("./routes/orderRoutes"); // Ensure the path is correct
+const orderRoutes = require("./routes/orderRoutes");
 const { API_ENDPOINTS } = require("./config/APIconfig");
 const { constants } = require("./config/constantsConfig");
 
-const PORT = process.env.PORT || constants.PORT; // Use a port from environment variable or constants
+const PORT = process.env.PORT || constants.PORT;
 const app = express();
-const server = http.createServer(app); // Create HTTP server instance
+const server = http.createServer(app);
+
+// CORS Options
+const corsOptions = {
+  origin: "https://bigbrew-app.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true,
+};
 
 // Initialize Socket.IO with CORS options
-const io = new Server(server, {
-  cors: {
-    origin: "https://bigbrew-app.vercel.app", // Allow requests from this origin
-    methods: ["GET", "POST"], // Allow specific methods
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  },
-});
+const io = new Server(server, { cors: corsOptions });
 
 // Middleware
-app.use(cors()); // Use CORS middleware
+app.use(cors(corsOptions)); // Apply CORS middleware
 app.use(express.json()); // Parse JSON bodies
 
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    // Start the server only after MongoDB connection is established
     server.listen(PORT, () => {
-      console.log(${constants.SUCCESS.SERVER} ${PORT});
+      console.log(`${constants.SUCCESS.SERVER} ${PORT}`);
     });
   })
   .catch((error) => {
-    console.error(${constants.ERROR.CONNECTION_FAILED}, error);
+    console.error(`${constants.ERROR.CONNECTION_FAILED}`, error);
   });
 
 // Middleware to attach io instance to request
@@ -49,13 +49,10 @@ app.use(API_ENDPOINTS.MAIN.DEFAULT, orderRoutes);
 
 // Handle socket connections
 io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id); // Log when a user connects
-
-  // Emit an event to the client confirming the connection
+  console.log("A user connected:", socket.id);
   socket.emit("connectionStatus", { status: "connected" });
 
-  // Handle disconnection
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id); // Log when a user disconnects
+    console.log("User disconnected:", socket.id);
   });
 });
